bec04f0cd9fd5253e0fa0d4a15fea313c21ddc4b
Geflucken <constantine992@gmail.com>
2019-07-10 17:49:13 +0300

    BT46 Setup Track Throttles

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -10,7 +10,7 @@
 // Sets default values
 ATank::ATank()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
 	// No need to protect pointers as added at construction
