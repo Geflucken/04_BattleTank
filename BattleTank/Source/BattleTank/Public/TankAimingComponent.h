a0bedd1745e3b4ce9e94f6414e0bc87647911ede
Geflucken <constantine992@gmail.com>
2019-07-31 18:27:58 +0300

    BT83 Using FAttachmentTransformRules

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -66,7 +66,7 @@ private:
 	UTankTurret* Turret = nullptr;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Firing")
-	float LaunchSpeed = 4000;
+	float LaunchSpeed = 20000;
 	
 	FVector AimDirection;
 	
