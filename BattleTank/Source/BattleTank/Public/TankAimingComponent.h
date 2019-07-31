1f983b0d112a4d44a75c75a65a46678768901d25
Geflucken <constantine992@gmail.com>
2019-07-31 16:42:12 +0300

    BT80 Preparing for Particles

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -40,7 +40,7 @@ public:
 	EFiringState GetFiringState() const;
 
 	UFUNCTION(BlueprintCallable, Category = "Firing")
-	int GetRoundsLeft() const;
+	int32 GetRoundsLeft() const;
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
@@ -70,7 +70,8 @@ private:
 	
 	FVector AimDirection;
 	
-	int RoundsLeft = 3;
+	UPROPERTY(EditDefaultsOnly, Category = "Firing")
+	int32 RoundsLeft = 3;
 
 	void MoveBarrelTowards();
 	bool IsBarrelMoving();
